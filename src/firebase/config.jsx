10b57890ecb1db73f1d25ef4e5a5cd9dd1242ddf
Michael Owuor <michael.owuor@gmail.com>
2022-09-19 20:01:39 +0300

    feat : getting documents (#4)

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 /** firebase configuration object containing keys and identifiers */
 const firebaseConfig = {
@@ -19,4 +19,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 /** export services */
-export { db };
+export { db, collection, getDocs };
