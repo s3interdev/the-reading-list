88008a7c7e8b51535ec099875cdf1000bf8c5b60
Michael Owuor <michael.owuor@gmail.com>
2022-09-20 13:38:20 +0300

    feat: firestore queries and security (#15)

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,11 @@
+import { useAuthContext } from '../../hooks/useAuthContext';
 import { useCollection } from '../../hooks/useCollection';
 import BookList from '../../components/book-list';
 import BookForm from '../../components/book-form';
 
 const Home = () => {
-	const { documents: books } = useCollection('books');
+	const { user } = useAuthContext();
+	const { documents: books } = useCollection('books', ['uid', '==', user.uid]);
 
 	return (
 		<div className="home">
